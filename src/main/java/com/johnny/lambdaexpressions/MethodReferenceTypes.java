98c77b39b59d5805fdebe6ee4b3a6aaf2f1d7ddc
johnny <john.robertson88@yahoo.com>
2024-01-30 23:52:50 -0800

    Add examples for staticMethodReferences and constructorMethodReferences

--- src/main/java/com/johnny/lambdaexpressions/MethodReferenceTypes.java
+++ src/main/java/com/johnny/lambdaexpressions/MethodReferenceTypes.java
@@ -1,16 +1,19 @@
 package com.johnny.lambdaexpressions;
 
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.*;
 
 public class MethodReferenceTypes{
 
     public static void main(String[] args) {
         MethodReferenceTypes mrt = new MethodReferenceTypes();
 //        mrt.BoundMethodReferences();
-        mrt.unboundedReferences();
+//        mrt.unboundedReferences();
+//        mrt.staticMethodReferences();
+        mrt.constructorMethodReferences();
     }
 
     public void BoundMethodReferences() {
@@ -55,7 +58,53 @@ public class MethodReferenceTypes{
         BiFunction<String, String, String> concatMR = String::concat;
         System.out.println(concatL.apply("Sean", "Kennedy"));
         System.out.println(concatMR.apply("Sean", "Kennedy"));
+    }
 
+    public static void staticMethodReferences(){
+        // Static method references are considered unbound also. An example
+        // static method is Collections.sort(List)
+        // Consumer<T>
+        //    void accept(T t)
+        // NB: Consumer takes one parameter => sort(List) is used, as opposed
+        // to sort(List, Comparator)
+        Consumer<List<Integer>> sortL = list -> Collections.sort(list);
+        Consumer<List<Integer>> sortMR = Collections::sort;
+
+        List<Integer> listOfNumbers = Arrays.asList(2, 1, 5, 4, 9);
+        sortL.accept(listOfNumbers);
+        System.out.println(listOfNumbers);
+
+        listOfNumbers = Arrays.asList(8, 12, 4, 3, 7);
+        sortMR.accept(listOfNumbers);
+        System.out.println(listOfNumbers);
+    }
+
+    public static void constructorMethodReferences(){
+        // Supplier<T>
+        //    T get()
+        Supplier<StringBuilder> sbL = () -> new StringBuilder();
+        Supplier<StringBuilder> sbMR = StringBuilder::new;
+        StringBuilder sb1 = sbL.get();
+        sb1.append("Lambda version");
+        System.out.println(sb1);
+
+        StringBuilder sb2 = sbMR.get();
+        sb2.append("Method Reference version");
+        System.out.println(sb2);
+
+        //Function<T, R>
+        //    R apply(T t)
+        //        List<String> apply(Integer)
+        // ArrayList(int initialCapacity)
+        Function<Integer, List<String>> alL = x -> new ArrayList(x);
+        Function<Integer, List<String>> alMR = ArrayList::new;
+        List<String> ls1 = alL.apply(10);
+        ls1.add("21");
+        System.out.println(ls1);
+
+        List<String> ls2 = alMR.apply(5);
+        ls2.add("88");
+        System.out.println(ls2);
     }
 }
 
