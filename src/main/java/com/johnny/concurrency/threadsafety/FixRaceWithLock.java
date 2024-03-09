4044039ecd3f7f0b9f3ff6577bf29274d93df15f
johnny <john.robertson88@yahoo.com>
2024-03-08 22:49:44 -0800

    Add lock example for thread safety

---
+++ src/main/java/com/johnny/concurrency/threadsafety/FixRaceWithLock.java
@@ -0,0 +1,34 @@
+package com.johnny.concurrency.threadsafety;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+public class FixRaceWithLock {
+    private static int count = 0;
+    private static Lock lock = new ReentrantLock();
+
+    public static void addToCounter(){
+        if(lock.tryLock()){
+            try{
+//                lock.lock(); // blocking call
+                int c = count;
+                System.out.println("Before. " + count + ". Thread Id: " + Thread.currentThread().getId());
+                count++;
+                System.out.println("After. " + count + ". Thread Id: " + Thread.currentThread().getId());
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            // Did not get the lock... do something else
+            System.out.println("Failed to get lock...");
+        }
+    }
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 10)
+                .forEach(i ->
+                        new Thread(FixRaceWithLock::addToCounter)
+                                .start());
+    }
+}
