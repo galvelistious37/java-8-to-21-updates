ea638dbf2bebda96a7ff533a2cdb142e09296c54
johnny <john.robertson88@yahoo.com>
2024-03-14 21:46:44 -0700

    Localization examples with languange and country

---
+++ src/main/java/com/johnny/localization/CreatingLocales.java
@@ -0,0 +1,31 @@
+package com.johnny.localization;
+
+import java.util.Locale;
+
+public class CreatingLocales  {
+    public static void main(String[] args) {
+        // 1. Constructors
+        System.out.println(new Locale("en"));
+        Locale locEnglishGB = new Locale("en", "GB");
+        System.out.println(locEnglishGB.getDisplayLanguage());
+        System.out.println(locEnglishGB.getDisplayCountry());
+
+        // 2. Built-in constants
+        System.out.println(Locale.FRENCH);
+        Locale locFrenchFrance = Locale.FRENCH;
+        System.out.println(locFrenchFrance.getDisplayLanguage());
+        System.out.println(locFrenchFrance.getDisplayCountry());
+
+        // 3. Locale.Builder() pattern
+        Locale locArabicEgypt = new Locale.Builder()
+                .setLanguage("ar")
+                .setRegion("EG")
+                .build();
+        System.out.println(locArabicEgypt);
+        Locale locArabicKuwait = new Locale.Builder()
+                .setRegion("KW")
+                .setLanguage("ar")
+                .build();
+        System.out.println(locArabicKuwait);
+    }
+}
