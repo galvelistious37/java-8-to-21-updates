12d4f197460e2157b5b4e5bfa2c2d77d81bcc08b
johnny <john.robertson88@yahoo.com>
2024-03-24 20:11:49 -0700

    Unnamed class and instance main method example

---
+++ src/main/java/com/johnny/unnamedclassesandinstancemainmethod/HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+int x = 5;
+
+void main(){
+    System.out.println("Hello World!");
+	showX();
+	new Other().test();
+}
+
+void showX(){
+	System.out.println(x);
+}
---
+++ src/main/java/com/johnny/unnamedclassesandinstancemainmethod/Other.java
@@ -0,0 +1,5 @@
+class Other{
+	void test(){
+			System.out.println("Test Other");
+	}
+}
